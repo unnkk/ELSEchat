16f79eb25749ee3068586384729c450bd5d19f4e
unnkk <unnyakmail@gmail.com>
2025-09-20 22:19:01 +0500

    Added Spring Boot skeleton

---
+++ src/main/java/com/unnkk/elsechat/ElsechatApplication.java
@@ -0,0 +1,13 @@
+package com.unnkk.elsechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unnkk/elsechat/ElsechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unnkk.elsechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
