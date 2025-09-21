1347e1e87dcd227f1afa418727f88a4c51d79cfa
unnkk <unnyakmail@gmail.com>
2025-09-21 13:09:42 +0500

    Added User entity and UserRepository to work with DB

---
+++ src/main/java/com/unnkk/elsechat/entities/User.java
@@ -0,0 +1,21 @@
+package com.unnkk.elsechat.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private String displayName;
+}
