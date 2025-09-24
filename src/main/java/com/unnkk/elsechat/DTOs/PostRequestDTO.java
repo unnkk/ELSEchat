2bc1616bb1830cd2c25fef73dd6c71f14612d3b8
unnkk <unnyakmail@gmail.com>
2025-09-24 23:19:17 +0500

    Bring posts entities and dtos to final form before adding functional

--- src/main/java/com/unnkk/elsechat/DTOs/PostRequestDTO.java
+++ src/main/java/com/unnkk/elsechat/DTOs/PostRequestDTO.java
@@ -4,6 +4,4 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record PostRequestDTO(
-        @NotBlank @Size(max = 500) String content,
-        @NotNull long author_id) {}
+public record PostRequestDTO(@NotBlank @Size(max = 500) String content) {}
--- src/main/java/com/unnkk/elsechat/entities/Post.java
+++ src/main/java/com/unnkk/elsechat/entities/Post.java
@@ -1,5 +1,6 @@
 package com.unnkk.elsechat.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -21,9 +22,11 @@ public class Post {
     private String content;
 
     @Column(nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ssXXX")
     private OffsetDateTime createdAt;
 
     @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ssXXX")
     private OffsetDateTime updatedAt;
 
     @PrePersist
