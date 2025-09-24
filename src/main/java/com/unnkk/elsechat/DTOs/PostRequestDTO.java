202aeb012f5b0dffdf1bc07574d8adf9f046e7b0
unnkk <unnyakmail@gmail.com>
2025-09-24 23:21:09 +0500

    bruh, unused import

--- src/main/java/com/unnkk/elsechat/DTOs/PostRequestDTO.java
+++ src/main/java/com/unnkk/elsechat/DTOs/PostRequestDTO.java
@@ -1,7 +1,6 @@
 package com.unnkk.elsechat.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record PostRequestDTO(@NotBlank @Size(max = 500) String content) {}
